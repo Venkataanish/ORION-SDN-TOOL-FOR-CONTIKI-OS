1098628b7a341d0a984cc84f8c0c08c43efdb957
root <root@instant-contiki.(none)>
2016-12-04 16:51:53 +0100

    DELETED MANY

--- examples/orion-mqtt-sn/project-conf.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROJECT_CONF_H_
-#define PROJECT_CONF_H_
-
-#define MQTT_DEMO_BROKER_IP_ADDR "fd00::1"
-/*---------------------------------------------------------------------------*/
-#endif
